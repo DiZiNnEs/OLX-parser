290e4e632ceaa17f33c08f4147f2eb9c28321abf
Iglek Kuat <dizinnes228@gmail.com>
2020-09-22 21:48:48 +0600

    fit: add cli

---
+++ src/cli.py
@@ -0,0 +1,15 @@
+import json
+
+
+class CLI:
+    def __init__(self):
+        pass
+
+    def read(self):
+        for json_files in ['result.json', 'result_2.json']:
+            with open(json_files) as f:
+                data = json.load(f)
+
+            json_data = json.dumps(data, indent=4, sort_keys=True, ensure_ascii=False).strip('\n')
+
+            print(json_data)
