851329fedfb32e4fd472361340c0b9496ecf6229
Iglek Kuat <dizinnes228@gmail.com>
2020-09-24 02:35:49 +0600

    refactor: add typing for methods and constructor

--- src/json.py
+++ src/json.py
@@ -1,5 +1,10 @@
 import json
-from typing import List
+from typing import (
+    List,
+    Dict,
+    Union,
+    Any
+)
 
 from .browser import Browser
 
@@ -8,7 +13,7 @@ class Json:
     def __init__(self, browser: Browser) -> None:
         self.browser = browser
 
-    def add(self, results: List[str]) -> None:
+    def add(self, results: List[Dict[str, Union[int, Any]]]) -> None:
         dates_to_json = json.dumps(results, indent=4, ensure_ascii=False)
 
         with open('result.json', 'w', encoding='utf8') as file:
