6ec6e2e3b204cf27001d38187b031de18d594073
Iglek Kuat <dizinnes228@gmail.com>
2020-09-23 21:40:21 +0600

    fit: add new method

--- src/json.py
+++ src/json.py
@@ -1,3 +1,13 @@
 import json
 from .browser import Browser
 
+
+class Json:
+    def __init__(self, browser: Browser):
+        self.browser = browser
+
+    def add(self, results) -> None:
+        dates_to_json = json.dumps(results, indent=4, ensure_ascii=False)
+
+        with open('result.json', 'w', encoding='utf8') as file:
+            file.write(dates_to_json)
