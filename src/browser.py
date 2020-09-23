beba88564bc12f0bfdd2ec0c5f162b3391721206
Iglek Kuat <dizinnes228@gmail.com>
2020-09-23 20:05:14 +0600

    refactor: add selenium method

--- src/browser.py
+++ src/browser.py
@@ -1,3 +1,4 @@
+from selenium import webdriver as wd
 from selenium import webdriver
 from selenium.common import exceptions as selenium_exceptions
 from bs4 import BeautifulSoup
@@ -13,7 +14,7 @@ class Browser:
     async def get_html(self, links):
         all_dates = []
         ad_number = 1
-        driver = webdriver.Chrome('/home/dizinnes/Downloads/chromedriver')
+        driver = wd.Chrome('/home/dizinnes/Downloads/chromedriver')
         for page_link in links:
             driver.get(page_link)
             try:
@@ -52,4 +53,31 @@ class Browser:
         return dates_to_json
 
     async def selenium(self, urls):
+        data = {}
         results = []
+        ad_number = 1
+        for page in urls:
+            self.webdriver.get(page)
+
+            try:
+                self.webdriver.find_element_by_css_selector('span.button.inverted.spoiler').click()
+                time.sleep(0.2)
+                soup = BeautifulSoup(self.webdriver.page_source, 'lxml')
+                data = {
+                    "Страница": 1,
+                    "Номер объявление": ad_number,
+                    "Название": soup.select_one('div.offer-titlebox > h1').get_text().strip().replace('\n', ''),
+                    "Имя продавца": soup.select_one('h4 > a').get_text().replace('\n', '').strip(),
+                    "Цена": soup.select_one('strong.pricelabel__value.not-arranged').get_text(),
+                    "Описание": soup.select_one('div.clr.lheight20.large').get_text().strip().replace('\n', ' '),
+                    "Номер телефона": soup.select_one('div.contactitem > strong').get_text() + ', ',
+                    "Ссылка на объявление": page,
+                }
+            except:
+                pass
+
+            print(data)
+            results.append(data)
+            ad_number += 1
+
+        return results
