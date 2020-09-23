342471073f7f4eae15b4fce28f383bc9b97a9989
Iglek Kuat <dizinnes228@gmail.com>
2020-09-23 15:09:03 +0600

    refactor: uncomment code in function html processing

--- src/html_handler.py
+++ src/html_handler.py
@@ -6,30 +6,28 @@ from .request import Request
 
 
 class HtmlHandler:
-    def __init__(self, request: Request, product: str) -> None:
+    def __init__(self, request: Request) -> None:
         """
         Constructor takes one parameter `request`
         :param request: Request
         """
         self.request = request
-        self.product = product
 
     async def html_processing(self) -> List[str]:
         """
         The method returns links to the  product in the List
         :return: List[str]
         """
-        # product = str(input('Ведите название продукта (htmlHandler): ')) # Потом откоментируй
+        product = str(input('Ведите название продукта (htmlHandler): '))  # Потом откоментируй
         links = []
-        soup = BeautifulSoup(await self.request.request(self.product), 'lxml')
+        soup = BeautifulSoup(await self.request.request(product), 'lxml')
         for html in soup.select('div.offer-wrapper'):
             url = html.select_one('a.marginright5.link.linkWithHash.detailsLink')['href']
             links.append(url)
         return links
 
-    async def get_links(self, html_product) -> None:
-        links = []
+    async def get_pages(self, html_product) -> None:
+        pages = []
         soup = BeautifulSoup(html_product, 'lxml')
-        for html in soup.select(''):
-            pass
-
+        for html in soup.select('div.pager.rel.clr>span.item.fleft'):
+            print(html)
