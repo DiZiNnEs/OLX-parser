635e55335bfac446a36d6f7717a3595a36abc988
Iglek Kuat <dizinnes228@gmail.com>
2020-09-26 14:37:51 +0600

    fit: add function for config.py

---
+++ src/config.py
@@ -0,0 +1,6 @@
+from selenium import webdriver
+
+user_agent = {
+    'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.102 Safari/537.36'
+}
+driver = webdriver.Chrome('/home/dizinnes/Downloads/chromedriver')
